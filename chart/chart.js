86316bb598df9c63113f63cefedc7bdf4c70eeb7
ma7hs <matheus.siqueira100@gmail.com>
2023-04-03 14:03:48 -0300

    teste

--- chart/chart.js
+++ chart/chart.js
@@ -3,25 +3,28 @@ const btn = document.getElementById('button')
 
 async function updateChart() {
   const fetchData = async () => {
-    const url = `http://localhost:8080/v1/lion-school/status/disciplinas/${20151001017}`
+    const url = `http://localhost:8080/v1/lion-school/status/disciplinas/${20151001004}`
     const response = await fetch(url)
     const data = await response.json()
     return data
   }
-
-  const data = await fetchData();
-  
+  const data = await fetchData()
   const disciplineName = data.disciplinas.map((index) => {
     return index.nome
   })
-
   const disciplineAverage = data.disciplinas.map((index) => {
     return index.media
   })
 
   let arrayColors = []
-  disciplineAverage.forEach(index => {
-    console.log(index)
+  disciplineAverage.forEach(mediaMateria => {
+    if(mediaMateria >= 0 && mediaMateria < 50){
+      arrayColors.push('rgba(193, 16, 16, 1)');
+    }else if(mediaMateria >= 50 && mediaMateria < 80){
+      arrayColors.push('rgba(229, 182, 87, 1)');
+    }else if(mediaMateria >= 80 && mediaMateria <= 100){
+      arrayColors.push('rgba(51, 71, 176, 1)');
+    }
   });
 
   const myChart = new Chart(ctx, {
@@ -31,23 +34,16 @@ async function updateChart() {
       datasets: [{
         label: `Media de Notas`,
         data: disciplineAverage,
-        borderWidth: 2,
+        borderWidth: 1,
         borderRadius: 10,
-        backgroundColor: [
-          'rgba(51, 71, 176, 1)',
-          'rgba(193, 16, 16, 1)',
-          'rgba(229, 182, 87, 1)',
-          'rgba(51, 71, 176, 1)',
-          'rgba(51, 71, 176, 1)',
-          'rgba(51, 71, 176, 1)',
-        ]
+        backgroundColor: arrayColors
       }]
     },
     options: {
       scales: {
         y: {
           beginAtZero: true
-        }
+        },
       }
     }
   })
