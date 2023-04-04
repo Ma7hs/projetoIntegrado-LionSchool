68b6a43444673cb61755756cceae60de62b52a40
Leticia Evelin <ljkeleticia@gmail.com>
2023-04-03 22:28:45 -0300

    inicio cards alunos

--- frontend/js/course.js
+++ frontend/js/course.js
@@ -1,20 +1,40 @@
 'use strict'
 
 import { fetchStudent } from "./api.js";
+import { fetchData } from "./api.js";
 
 var id = localStorage.getItem('id_card')
 const courseStudents = await fetchStudent(id);
 
+const courseTitle = await fetchData();
+
+
+// const createTitle = (curso) => {
+//     const titleCourse = document.createElement('h1')
+//     titleCourse.classList.add('course__title')
+//     titleCourse.textContent = curso.nome
+
+//     titleCourse.append(titleCourse)
+//     return titleCourse
+// }
+
+// createTitle()
+// console.log(createTitle)
 
 const createCardAluno = (aluno) => {
-    const card = document.createElement('div')
-    card.classList.add('students__card')
 
+    // criando card alunos
+    const card = document.createElement('div')
+    if(aluno.status == "Finalizado"){
+        card.classList.add('students__finished')
+    } else {
+        card.classList.add('students__studying')
+    }
     const img = document.createElement ( 'img' )
     img.classList.add('students__image')
     img.src = aluno.foto
 
-    const name = document.createElement('h1')
+    const name = document.createElement('h3')
     name.classList.add('students__name')
     name.textContent = aluno.nome
     
