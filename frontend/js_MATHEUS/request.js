9cec02dd9bd7710a31b485807f50b25435d366c4
Matheus Siqueira Silva <87048682+Ma7hs@users.noreply.github.com>
2023-04-05 00:23:46 -0300

    Delete frontend/js_MATHEUS directory

--- frontend/js_MATHEUS/request.js
+++
@@ -1,7 +0,0 @@
-export const fetchData = async () => {
-    const url = `http://localhost:8080/v1/lion-school/cursos`
-    const response = await fetch(url)
-    const data = await response.json()
-    return {...data}
-}
-
