420a21f81c27c64ef499c6b8689f2da7614a99ef
Ma7hs <Matheus.siqueira100@gmail.com>
2023-04-13 08:28:42 -0300

    tst

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,14 +1,16 @@
 'use strict'
 
 export const fetchData = async () => {
-    const url = `https://lion-school.onrender.com/v1/lion-school/cursos`
+    // const url = `https://lion-school.onrender.com/v1/lion-school/cursos`
+    const url = `http://localhost:8080/v1/lion-school/cursos`
     const response = await fetch(url)
     const data = await response.json()
     return {...data}
 }
 
 export const fetchStudent = async (turma, status) => {
-    const url = `https://lion-school.onrender.com/v1/lion-school/${turma}/alunos/${status}`
+    // const url = `https://lion-school.onrender.com/v1/lion-school/${turma}/alunos/${status}`
+    const url = `http://localhost:8080/v1/lion-school/${turma}/alunos/${status}`
     const response = await fetch(url)
     const data = await response.json()
     return {...data}
